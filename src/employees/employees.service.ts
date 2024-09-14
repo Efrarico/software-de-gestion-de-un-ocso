e0c543225fea142fa84f3757b7d52212a41643fd
Efrain Rico Galvan <erico21@alumnos.uaq.mx>
2024-09-14 16:21:07 -0600

    resguardo

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import {v4 as uuid} from "uuid";
+
+@Injectable()
+export class EmployeesService {
+  private employees: CreateEmployeeDto[] = [
+  {
+    id: uuid(),
+    name: 'Alberto',
+    lastName: 'Gonzalez',
+    phoneNumber: '1234567890',
+  },
+  {
+    id: uuid(),
+    name: 'Juan',
+    lastName: 'Perez',
+    phoneNumber: '0987654321',
+  }
+];
+
+  create(createEmployeeDto: CreateEmployeeDto) {
+    createEmployeeDto.id = uuid();
+    this.employees.push(createEmployeeDto);
+
+    return createEmployeeDto;
+  }
+
+  findAll() {
+    return this.employees;
+  }
+
+  findOne(id: string) {
+    const employee = this.employees.filter((employee) => employee.id === id)[0]; 
+    if (!employee) throw new NotFoundException();
+    return employee;
+  }
+
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
+    let employeeToUpdate = this.findOne(id);
+    employeeToUpdate = {...employeeToUpdate, ...updateEmployeeDto};
+    if (employeeToUpdate) throw new NotFoundException();{
+      
+    }
+    this.employees = this.employees.map((employee) => {
+      if (employee.id === id) {
+        employee = employeeToUpdate; 
+      }
+      return employee;
+    });
+    return employeeToUpdate;
+  }
+
+  remove(id: string) {
+  this.findOne(id);
+    this.employees = this.employees.filter((employee) => employee.id !== id);
+    return this.employees;
+  }
+}

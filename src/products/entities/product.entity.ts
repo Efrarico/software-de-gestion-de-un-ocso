990b27f87b9e0594c7462df6a31c2dc68cf7a407
Efrain Rico Galvan <erico21@alumnos.uaq.mx>
2024-09-27 13:01:52 -0600

    better get methods

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn,  } from "typeorm";
+import { Provider } from "src/providers/entities/provider.entity";
+import { Entity,ManyToOne, Column, PrimaryGeneratedColumn,  } from "typeorm";
+
 
 @Entity()
 export class Product {
@@ -10,7 +12,9 @@ export class Product {
     price: number;
     @Column({type: "int"})
     countSeal: number;
-    // @Column({type: "uuid"})
-   // provider: string;
+   @ManyToOne(() => Provider, (provider) => Provider.products, {
+    eager: true
+   })
+        provider: Provider
 }
 
